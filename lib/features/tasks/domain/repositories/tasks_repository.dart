892481e29bde75e32d89e3106fff9a719113db04
JulianRivers <juliancamilorifo@ufps.edu.co>
2025-07-13 09:50:38 -0500

    add task repository

---
+++ lib/features/tasks/domain/repositories/tasks_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_offline/core/errors/failures.dart';
+
+
+abstract class TasksRepository {
+  Stream<Either<AppFailure, bool>> getTask(String id);
+  Stream<Either<AppFailure, List<Task>>> getTasksStream();
+  Future<Either<AppFailure, bool>> addTask(Task task);
+  Future<Either<AppFailure, bool>> deleteTask(String id);
+  Future<Either<AppFailure, bool>> updateTask(Task task);
+}
