516ceab5c2ab111da3253f43da59f47cb32cd5cd
JulianRivers <juliancamilorifo@ufps.edu.co>
2025-07-15 13:42:23 -0500

    create file servcie locator

---
+++ lib/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+Future<void> serviceLocatorInit() async {
+  //Bloc
+
+  //Use Cases
+
+  //Repository
+
+  //Data Sources
+
+  //Firebase
+  // getIt.registerLazySingleton<FirebaseModule>(
+  //   () => FirebaseImpl(firebaseAuth: getIt(), firestore: getIt()),
+  // );
+}
