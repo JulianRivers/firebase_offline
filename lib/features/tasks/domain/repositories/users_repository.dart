d05c3d492cce9d7474afa027b51dc2efb97156d2
JulianRivers <juliancamilorifo@ufps.edu.co>
2025-07-17 09:11:21 -0500

    add user repository

---
+++ lib/features/tasks/domain/repositories/users_repository.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_offline/core/errors/failures.dart';
+import 'package:firebase_offline/features/tasks/domain/entities/user.dart';
+
+abstract class UsersRepository {
+  Future<Either<AppFailure, User>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+  Future<Either<AppFailure, User>> signInWithEmail(
+    String emial,
+    String password,
+  );
+  Future<Either<AppFailure, User>> signInWithGoogle();
+  Future<Either<AppFailure, User>> signInWithAnonymous();
+  Future<Either<AppFailure, void>> logout();
+}
